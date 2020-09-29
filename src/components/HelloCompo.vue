c0f225b13117bc32db72769a90bd6afbe8cac662
Keinahas <kaidrielle@gmail.com>
2020-09-29 16:40:43 +0900

    20200921 seminar

---
+++ src/components/HelloCompo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    {{ name }}님 안녕하세요.<br />
+    받은 메시지: {{ value }}
+    <!-- v-on (= @) -->
+    <v-btn @click="onClick">Click Me</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["value", "name"],
+  data: () => ({
+    localName: "PSW",
+  }),
+  methods: {
+    onClick() {
+      // this.localName = "Clicked";
+      // console.log(this.localName);
+      this.$emit("on-btn");
+    },
+  },
+};
+</script>
+
+<style></style>
